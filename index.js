fa9a3761ad1565cccb16f526c8c7ae05a355a753
Edward Sutton <edward.sutton@subsite.com>
2017-11-08 08:04:29 -0600

    add todo

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 #!/usr/bin/env node
 
 // See: https://medium.freecodecamp.org/writing-command-line-applications-in-nodejs-2cf8327eee2
+// https://github.com/tj/commander.js
+
 
 'use strict';
 
 const program = require('commander');
 
+let _googleTranslateApiKey;
+let _workingFolder;
+let _inputLanguage;
+let _outputLanguageList;
+
 program
 .version('0.0.1')
-.command('i18n-translate-qt <googleTranslateApiKey> <workingFolder> <inputLanguage> <outputLanguageList>','\nUse Google Translate on Qt Linguist TS files')
+.description('Use Google Translate API to translate Qt Linguist TS files')
+.usage('<googleTranslateApiKey> <workingFolder> <inputLanguage> <outputLanguageList>')
+.arguments('<googleTranslateApiKey> <workingFolder> <inputLanguage> <outputLanguageList>','\nUse Google Translate on Qt Linguist TS files')
 .action(function (googleTranslateApiKey, workingFolder, inputLanguage, outputLanguageList) {
   console.log('googleTranslateApiKey:', googleTranslateApiKey);
   console.log('workingFolder:', workingFolder);
   console.log('inputLanguage:', inputLanguage);
-  if (outputLanguageList) {
-    console.log('outputLanguageList:', outputLanguageList);
-    const languageList = outputLanguageList.split(',');
-    for(let i = 0; i < languageList.length; i += 1) {
-      console.log(`language[${i}] = `, languageList[i]);
-    }
-  }  
+
+  _googleTranslateApiKey = googleTranslateApiKey;
+  _workingFolder = workingFolder;
+  _inputLanguage = inputLanguage;
+  _outputLanguageList = outputLanguageList;
+
 }); 
 
 program.parse(process.argv);
 
 
-console.log('dbg: program.args.length:', program.args.length);
+const languageList = _outputLanguageList ? _outputLanguageList.split(',') : [];
+console.log('_outputLanguageList:', _outputLanguageList);
+for(let i = 0; i < languageList.length; i += 1) {
+  console.log(`language[${i}] = `, languageList[i]);
+}
 
-// if program was called with no arguments, show help.
-if (program.args.length < 4) program.help();
-
-console.log('you ordered a pizza with:');
+console.log('ToDo: 1) Iterate <inputLanguage> TS file, 2) For each language translate, 3) Build new language TS file');
 
